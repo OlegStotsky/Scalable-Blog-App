bc3f58f9ed6e5b1cde9ad002c9b04d02810e9b1e
Oleg Stotsky <ostotsky@gmail.com>
2018-10-08 19:41:46 +1000

    add cleanCache middleware, apply it to post blog route

--- services/cache.js
+++ services/cache.js
@@ -10,15 +10,15 @@ client.hset = util.promisify(client.hset);
 
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.cache = async function(options={}) {
-    this.useCache = true;
-    this.hashKey = JSON.stringify(options.key || '');
-    return this;
-}
+mongoose.Query.prototype.cache = async function(options = {}) {
+  this.useCache = true;
+  this.hashKey = JSON.stringify(options.key || "");
+  return this;
+};
 
 mongoose.Query.prototype.exec = async function() {
   if (!this.useCache) {
-      return exec.apply(this, arguments);
+    return exec.apply(this, arguments);
   }
 
   const cacheKey = JSON.stringify(
@@ -29,16 +29,28 @@ mongoose.Query.prototype.exec = async function() {
   const cacheValue = await client.hget(this.hashKey, cacheKey);
   const maybeCacheValue = cacheValue ? Maybe.Just(cacheValue) : Maybe.Nothing();
   return maybeCacheValue.matchWith({
-        Just: ({ value }) => {
-          const doc = JSON.parse(value);
-          return Array.isArray(doc)
-            ? doc.map(x => new this.model(x))
-            : new this.model(doc);
-        },
-        Nothing: async () => {
-          const queryResult = await exec.apply(this, arguments);
-          await client.hset(this.hashKey, cacheKey, JSON.stringify(queryResult), 'EX', 10);
-          return queryResult;
-        }
-      });
+    Just: ({ value }) => {
+      const doc = JSON.parse(value);
+      return Array.isArray(doc)
+        ? doc.map(x => new this.model(x))
+        : new this.model(doc);
+    },
+    Nothing: async () => {
+      const queryResult = await exec.apply(this, arguments);
+      await client.hset(
+        this.hashKey,
+        cacheKey,
+        JSON.stringify(queryResult),
+        "EX",
+        10
+      );
+      return queryResult;
+    }
+  });
+};
+
+module.exports = {
+  clearHash(hashKey) {
+    client.del(JSON.stringify(hashKey));
+  }
 };
