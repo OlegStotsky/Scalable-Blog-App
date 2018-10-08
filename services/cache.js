d0f1c20807bc291b30e1518284a282d218cb3fa2
Oleg Stotsky <ostotsky@gmail.com>
2018-10-08 17:56:29 +1000

    refactor cache service to use Maybe monad

--- services/cache.js
+++ services/cache.js
@@ -1,8 +1,9 @@
-const mongoose = require('mongoose');
-const redis = require('redis');
-const util = require('util');
+const mongoose = require("mongoose");
+const redis = require("redis");
+const util = require("util");
+const Maybe = require('folktale/maybe');
 
-const redisUrl = 'redis://localhost:6379';
+const redisUrl = "redis://localhost:6379";
 const client = redis.createClient(redisUrl);
 client.get = util.promisify(client.get);
 client.set = util.promisify(client.set);
@@ -10,15 +11,19 @@ client.set = util.promisify(client.set);
 const exec = mongoose.Query.prototype.exec;
 
 mongoose.Query.prototype.exec = async function() {
-    const cacheKey = JSON.stringify(Object.assign({}, this.getQuery(), { collection: this.mongooseCollection.name }));
-    const cacheValue = await client.get(cacheKey);
-    if (cacheValue) {
-        console.log(cacheValue);
-        return JSON.parse(cacheValue);
-    }
-
-    const queryResult = await exec.apply(this, arguments);
-    console.log(queryResult);
-    await client.set(cacheKey, JSON.stringify(queryResult));
-    return queryResult;
-}
+  const cacheKey = JSON.stringify(
+    Object.assign({}, this.getQuery(), {
+      collection: this.mongooseCollection.name
+    })
+  );
+  const cacheValue = await client.get(cacheKey);
+  return cacheValue ? Maybe.Just(cacheValue) : Maybe.Nothing()
+       .matchWith({
+           Just: ({ value }) => JSON.parse(value),
+           Nothing: async () => {
+              const queryResult = await exec.apply(this, arguments);
+              await client.set(cacheKey, JSON.stringify(queryResult));
+              return queryResult;
+           }
+       });
+};
