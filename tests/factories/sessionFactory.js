afe92e23d8c2c2293df152b303c869ac1905a77c
Oleg Stotsky <ostotsky@gmail.com>
2018-10-10 17:12:43 +1000

    add user factory; add session factory

---
+++ tests/factories/sessionFactory.js
@@ -0,0 +1,16 @@
+const buffer = require('safe-buffer').Buffer;
+const keys = require('../../config/keys');
+const Keygrip = require('keygrip');
+const keygrip = Keygrip([keys.cookieKey]);
+
+module.exports = (user) => {
+  const sessionObject = {
+    passport: {
+      user: user._id.toString()
+    }
+  };
+  const session = Buffer.from(JSON.stringify(sessionObject)).toString('base64');
+  const sig = keygrip.sign('session=' + session);
+
+  return { session, sig };
+};
